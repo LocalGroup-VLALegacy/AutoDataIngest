6c37d3dda968676176d0b206f610285b123a199c
e-koch <koch.eric.w@gmail.com>
2020-05-19 23:24:57 -0700

    Outline template job for custom continuum pipeline

--- job_templates/job_continuum_pipeline.py
+++ job_templates/job_continuum_pipeline.py
@@ -53,3 +53,50 @@ echo "casa default continuum pipeline finished."
         '''
 
     return job_str
+
+
+def cedar_submission_script(target_name="M31",
+                            config="C",
+                            trackname="20A-346.sb38098105.eb38158028.58985.68987263889",
+                            slurm_kwargs={},
+                            setup_kwargs={},
+                            conditional_on_jobnum=None):
+    '''
+    Runs the default VLA pipeline.
+
+    TODO: Make job start conditional on the split job finishing (need to pass that job num)
+    '''
+
+    slurm_kwargs['job_name'] = f"{target_name}_{config}_{trackname}"
+    slurm_kwargs['job_type'] = "continuum_pipeline_default"
+
+    if conditional_on_jobnum is not None:
+        slurm_kwargs['dependency'] = f"afterok:{conditional_on_jobnum}"
+
+    slurm_str = cedar_slurm_setup(**slurm_kwargs)
+    setup_str = cedar_job_setup(**setup_kwargs)
+
+    job_str = \
+        f'''{slurm_str}\n{setup_str}
+
+export TRACK_FOLDER="{target_name}_{config.upper()}_{trackname}"
+
+cd /home/ekoch/scratch/VLAXL_reduction/$TRACK_FOLDER
+
+# Copy the rcdir here and append the pipeline path
+cp -r ~/.casa .
+echo "sys.path.append('/home/ekoch/scratch/VLAXL_reduction/$TRACK_FOLDER/ReductionPipeline/')" >> .casa/init.py
+
+# Move into the continuum pipeline
+
+cd $TRACK_FOLDER"_continuum"
+
+echo 'Start casa default continuum pipeline'
+
+xvfb-run -a ~/casa-pipeline-release-5.6.2-3.el7/bin/casa --rcdir ../.casa --nologger --nogui --log2term --nocrashreport --pipeline -c ../ReductionPipeline/lband_pipeline/continuum_pipeline.py {trackname}.continuum.ms
+
+echo "casa default continuum pipeline finished."
+
+        '''
+
+    return job_str
