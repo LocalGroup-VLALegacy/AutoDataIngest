ef40e33635668ca6caee559a32ecc3438a28353c
e-koch <koch.eric.w@gmail.com>
2021-04-13 19:47:18 -0400

    Add more notes to the data manager instance

--- setup_environment.sh
+++ setup_environment.sh
@@ -18,7 +18,7 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 pip install --upgrade --user globus-cli
 
 # These install into the .local/bin folder
-echo "export PATH=$PATH:/home/ekoch/.local/bin" >> ~/.bashrc
+echo "export PATH=$PATH:/home/datamanager/.local/bin" >> ~/.bashrc
 source ~/.bashrc
 
 # Update globus (if needed)
@@ -28,14 +28,16 @@ globus update
 pip install --upgrade --user vofs
 
 # We also want some astropy packages
-conda install -y astropy
+conda install -y astropy ipython numpy scipy matplotlib
 
 # There's a bug fix we need from astroquery that is only in the
 # dev version as of 10/14/20
-git clone https://github.com/astropy/astroquery.git
-cd astroquery
-pip install -e .
-cd ../
+# git clone https://github.com/astropy/astroquery.git
+# cd astroquery
+# pip install -e .
+# cd ../
+# Should be able to use release v0.4 as of 04/2021
+pip install astroquery
 
 # Eventually we'll need plotly, too
 conda install -y plotly
@@ -48,6 +50,7 @@ wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodrive
 tar zxf geckodriver-v0.26.0-linux64.tar.gz
 # executable now in home directory, move to somewhere in PATH
 mv geckodriver .local/bin/
+rm -rf geckodriver-v0.26.0*.tar.gz
 
 
 # And selenium
@@ -67,7 +70,8 @@ pip install ezgmail
 # Unsure why enum34 is getting installed for py >3.7...
 
 # Like cron, but in python and easier to use:
-pip install schedule
+# Not using right now.
+# pip install schedule
 
 # fabric handles ssh connections in python
 pip install fabric
@@ -77,6 +81,24 @@ pip install async_timeout
 
 # Install globus here. This require some manual steps to setup the endpoint.
 # But it need to be running at all times:
-cd globusconnectpersonal-3.1.2
+sudo apt-get install tk tcllib
+$ wget https://downloads.globus.org/globus-connect-personal/linux/stable/globusconnectpersonal-latest.tgz
+tar -xf globusconnectpersonal-latest.tgz
+cd globusconnectpersonal-3.1.4
+# Do the first time setup
+./globusconnectpersonal -setup
+# YOU NEED PY37 for this to work currently! Breaks with py38...
 ./globusconnectpersonal -start &
 cd ../
+
+# You need the service_account.json file setup for gspread:
+# Will go into .local/gspread/service_account.json
+# Ask Eric about the account credentials attached to the shared gdrive
+
+# Next, setup ezgmail:
+# Will need oath2 credentials created.
+# See https://ezgmail.readthedocs.io/en/latest/
+# If token.json and credentials.json are in the home directory,
+# this should work:
+python -c "import ezgmail; ezgmail.init()"
+
