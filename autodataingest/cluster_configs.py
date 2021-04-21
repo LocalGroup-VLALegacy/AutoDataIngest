b7d3c9ac205598ce8e40464dafeb7ca87676e05e
e-koch <koch.eric.w@gmail.com>
2021-04-21 17:38:26 -0400

    Update ingester and globus endpoint

--- autodataingest/cluster_configs.py
+++ autodataingest/cluster_configs.py
@@ -16,8 +16,8 @@ ENDPOINT_INFO = {'cc-cedar': {'endpoint_id': "c99fd40c-5545-11e7-beb6-22000b9a44
                               'data_path': '/lustre/aoc/projects/20A-346/data_staged/'},
                  'msu-hpcc': {'endpoint_id': "a640bafc-6d04-11e5-ba46-22000b92c6ec",
                              'data_path': "/mnt/research/ChomiukLab/LocalGroupX/M31_20A-346/"},
-                 'ingester': {'endpoint_id': "ad5427e4-1027-11eb-81b1-0e2f230cc907",
-                              'data_path': "/home/ekoch/"}}
+                 'ingester': {'endpoint_id': "e8fc98cc-9ca8-11eb-92cd-6b08dd67ff48",
+                              'data_path': "/home/datamanager/"}}
 
 JOB_CREATION_FUNCTIONS = \
     {'cc-cedar': {'IMPORT_SPLIT': jobs_import.cedar_submission_script,
