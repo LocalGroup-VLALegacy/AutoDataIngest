1d553ca14e07ea24eeb88280c299526f0414b633
e-koch <koch.eric.w@gmail.com>
2021-04-22 19:11:28 -0400

    Make a folder version w/o an extension

--- autodataingest/utils.py
+++ autodataingest/utils.py
@@ -10,3 +10,14 @@ def uniquify(path):
         counter += 1
 
     return path
+
+
+def uniquify_folder(path):
+
+    counter = 1
+
+    while os.path.exists(path):
+        path = f"{path}_{str(counter)}"
+        counter += 1
+
+    return path
