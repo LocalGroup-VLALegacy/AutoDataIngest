942154792a008539cfc4dc76dd2c8303e0c4ea5a
e-koch <koch.eric.w@gmail.com>
2021-04-25 23:09:35 -0400

    Disable debug by default

--- main.py
+++ main.py
@@ -162,7 +162,7 @@ if __name__ == "__main__":
 
         loop = asyncio.new_event_loop()
 
-        loop.set_debug(True)
+        loop.set_debug(False)
         loop.slow_callback_duration = 0.001
 
         loop.run_until_complete(run(test_case_run_newest=test_case_run_newest,
