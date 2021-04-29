66b5e25cf92aa303ea3ca24c6dedba191ab5c30f
e-koch <koch.eric.w@gmail.com>
2021-04-29 19:14:59 -0400

    Allow setting which data type to split out

--- autodataingest/job_templates/job_import_and_merge.py
+++ autodataingest/job_templates/job_import_and_merge.py
@@ -11,6 +11,7 @@ from .job_tools import (cedar_slurm_setup, cedar_job_setup,
 
 def cedar_submission_script(target_name="M31", config="C",
                             trackname="20A-346.sb38098105.eb38158028.58985.68987263889",
+                            split_type='all',
                             slurm_kwargs={},
                             setup_kwargs={},
                             run_casa6=True):
@@ -41,7 +42,7 @@ echo "sys.path.append('/home/ekoch/scratch/VLAXL_reduction/$TRACK_FOLDER/Reducti
 
 echo 'Start casa'
 
-~/casa-6.1.2-7-pipeline-2020.1.0.36/bin/casa --rcdir .casa --nologger --nogui --log2term --nocrashreport --pipeline -c ReductionPipeline/lband_pipeline/ms_split.py {trackname} all
+~/casa-6.1.2-7-pipeline-2020.1.0.36/bin/casa --rcdir .casa --nologger --nogui --log2term --nocrashreport --pipeline -c ReductionPipeline/lband_pipeline/ms_split.py {trackname} {split_type}
 
 # Copy manual flag files if they're present:
 if [ -f manual_flagging_speclines.txt ]; then
