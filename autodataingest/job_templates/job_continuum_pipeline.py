3b50c53aee2a26c1c2c570c15178b6e803a0ffb4
e-koch <koch.eric.w@gmail.com>
2021-05-12 07:40:45 -0400

    Missed adding continuum job file

--- autodataingest/job_templates/job_continuum_pipeline.py
+++ autodataingest/job_templates/job_continuum_pipeline.py
@@ -142,6 +142,10 @@ echo 'Start casa default continuum pipeline'
 
 ~/casa-6.1.2-7-pipeline-2020.1.0.36/bin/casa --rcdir ../.casa --nologger --nogui --log2term --nocrashreport --pipeline -c ../ReductionPipeline/lband_pipeline/continuum_pipeline.py {trackname}.continuum.ms
 
+# Trigger an immediate re-run attempt: This will skip completed parts and QA txt files.
+# It's here because repeated plotms calls seem to stop working after awhile.
+~/casa-6.1.2-7-pipeline-2020.1.0.36/bin/casa --rcdir ../.casa --nologger --nogui --log2term --nocrashreport --pipeline -c ../ReductionPipeline/lband_pipeline/continuum_pipeline.py {trackname}.continuum.ms
+
 export exitcode=$?
 if [ $exitcode -ge 1 ]; then
     echo "Non-zero exit code from CASA. Exiting"
