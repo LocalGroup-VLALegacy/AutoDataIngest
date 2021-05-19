50db343db25d7c1e47ad265aeb6d0a0c461872c5
e-koch <koch.eric.w@gmail.com>
2021-05-19 16:36:20 -0400

    Add a timelimit handler for ssh connections timing out or otherwise getting stuck

--- autodataingest/ssh_utils.py
+++ autodataingest/ssh_utils.py
@@ -5,6 +5,8 @@ Functions for ssh loging and run using the fabric and paramiko paxkages.
 
 import fabric
 import paramiko
+import signal
+from contextlib import contextmanager
 
 
 def try_run_command(connect, test_cmd='ls'):
@@ -35,3 +37,17 @@ def run_command(connect, cmd, test_connection=False):
         raise ValueError(f"Failed to clone pipeline! See stderr: {result.stderr}")
 
     return result
+
+class TimeoutException(Exception):
+    pass
+
+@contextmanager
+def time_limit(seconds):
+    def signal_handler(signum, frame):
+        raise TimeoutException("Timed out!")
+    signal.signal(signal.SIGALRM, signal_handler)
+    signal.alarm(seconds)
+    try:
+        yield
+    finally:
+        signal.alarm(0)
