3f3ed67787a96437c8b31d4e283863e69743d27c
e-koch <koch.eric.w@gmail.com>
2021-06-04 10:04:58 -0400

    More cron uploader script fixes

--- sync_to_gdrive.sh
+++ sync_to_gdrive.sh
@@ -20,15 +20,13 @@ cd /home/datamanager/space/vlaxl/calibrated/
 source_ep=c99fd40c-5545-11e7-beb6-22000b9a448b
 dest_ep=e8fc98cc-9ca8-11eb-92cd-6b08dd67ff48
 
-export globus=/home/datamanager/.local/bin/globus
-
 # Get current list of MS files on the drive after deleting the previous list.
 rm drive_files.txt
 rclone lsf vlaxl_gdrive:MeasurementSets >> drive_files.txt
 
 # Get list of MS files on project space
 rm cedar_files.txt
-globus ls $source_ep:projects/rrg-eros-ab/ekoch/VLAXL/calibrated/ >> cedar_files.txt
+/home/datamanager/.local/bin/globus ls $source_ep:projects/rrg-eros-ab/ekoch/VLAXL/calibrated/ >> cedar_files.txt
 
 if cmp -s drive_files.txt cedar_files.txt; then
     echo "No new MS files found"
@@ -53,10 +51,10 @@ echo "Found files to upload: ${FILENAMES}"
 # Transfer to QA webserver
 echo "Transferring to QA server at $(date)"
 
-task_id="$(globus transfer $source_ep:projects/rrg-eros-ab/ekoch/VLAXL/calibrated/ $dest_ep:space/vlaxl/calibrated/ --jmespath 'task_id' --format=UNIX --batch < batch_files.txt)"
+task_id="$(/home/datamanager/.local/bin/globus transfer $source_ep:projects/rrg-eros-ab/ekoch/VLAXL/calibrated/ $dest_ep:space/vlaxl/calibrated/ --jmespath 'task_id' --format=UNIX --batch < batch_files.txt)"
 
 echo "Waiting on 'globus transfer' task '$task_id'"
-globus task wait "$task_id" --polling-interval 300
+/home/datamanager/.local/bin/globus task wait "$task_id" --polling-interval 300
 if [ $? -eq 0 ]; then
     echo "$task_id completed successfully";
 else
@@ -72,7 +70,7 @@ for filename in $FILENAMES; do
     # Upload to gdrive
     echo "Uploading ${filename} to gdrive at $(date)"
     flock -n /tmp/google_drv_sync.lock /usr/bin/rclone copy --retries 5 $filename vlaxl_gdrive:MeasurementSets/
-    echo "Finsihed uploading ${filename} to gdrive at $(date)"
+    echo "Finished uploading ${filename} to gdrive at $(date)"
 
     echo "Removing ${filename} at $(date)"
     rm $filename
