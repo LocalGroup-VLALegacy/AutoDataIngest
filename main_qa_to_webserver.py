98a5f9c775d64bf4cb2fb4ff2859d6d97d4d0984
e-koch <koch.eric.w@gmail.com>
2021-12-27 19:18:59 -0500

    Various updates to the manual handling of job transfer + completion

--- main_qa_to_webserver.py
+++ main_qa_to_webserver.py
@@ -22,6 +22,9 @@ from autodataingest.gsheet_tracker.gsheet_functions import (update_track_status)
 
 from autodataingest.ingest_pipeline_functions import AutoPipeline
 
+from autodataingest.logging import setup_logging
+log = setup_logging()
+
 
 async def produce(queue, sleeptime=60, start_with_newest=False,
                   ebid_list=None):
@@ -37,24 +40,26 @@ async def produce(queue, sleeptime=60, start_with_newest=False,
     if start_with_newest:
         all_ebids = all_ebids[::-1]
 
-    for ebid in all_ebids:
+    for ebid_plus_types in all_ebids:
         # produce an item
+        ebid, run_continuum, run_lines = ebid_plus_types
+
         print(f'Found new track with ID {ebid}')
 
         # Put a small gap between starting to consume processes
         await asyncio.sleep(sleeptime)
 
         # put the item in the queue
-        await queue.put(AutoPipeline(ebid, sheetname=SHEETNAME))
+        await queue.put([AutoPipeline(ebid, sheetname=SHEETNAME), run_continuum, run_lines])
 
 
 async def consume(queue, sleeptime=60):
     while True:
         # wait for an item from the producer
-        auto_pipe = await queue.get()
+        auto_pipe, RUN_CONTINUUM, RUN_LINES = await queue.get()
 
         # process the item
-        print('Processing {}...'.format(auto_pipe.ebid))
+        log.info('Processing {}...'.format(auto_pipe.ebid))
         # simulate i/o operation using sleep
         # await asyncio.sleep(1)
 
@@ -70,6 +75,13 @@ async def consume(queue, sleeptime=60):
 
             for data_type in data_types:
 
+                log.info(f"Transferring {data_type}")
+
+                if DO_DATA_TRANSFER:
+                    await auto_pipe.transfer_calibrated_data(data_type=data_type,
+                                                            clustername='cc-cedar')
+                    # continue
+
                 # Move pipeline products to QA webserver
                 await auto_pipe.transfer_pipeline_products(data_type=data_type,
                                                         startnode=CLUSTERNAME,
@@ -77,14 +89,24 @@ async def consume(queue, sleeptime=60):
 
                 await asyncio.sleep(sleeptime)
 
+                log.info(f"Creating flagging sheet for {data_type} (if needed)")
+
                 # Create the flagging sheets in the google sheet
                 # await auto_pipe.make_flagging_sheet(data_type='continuum')
                 await auto_pipe.make_flagging_sheet(data_type=data_type)
 
                 # Create the final QA products and move to the webserver
+                log.info(f"Creating QA products")
                 auto_pipe.make_qa_products(data_type=data_type)
 
+                log.info(f"Updating track status")
+                update_track_status(auto_pipe.ebid, message=f"Ready for QA",
+                                    sheetname=auto_pipe.sheetname,
+                                    status_col=1 if data_type == 'continuum' else 2)
+
                 await asyncio.sleep(sleeptime)
+
+                log.info(f"Finished {data_type}")
 
         # Notify the queue that the item has been processed
         queue.task_done()
@@ -104,23 +126,49 @@ async def run(**produce_kwargs):
 
 if __name__ == "__main__":
 
+    import logging
+    from datetime import datetime
+
+    LOGGER_FORMAT = '%(asctime)s %(message)s'
+    DATE_FORMAT = '[%Y-%m-%d %H:%M:%S]'
+    logging.basicConfig(format=LOGGER_FORMAT, datefmt=DATE_FORMAT)
+
+    log = logging.getLogger()
+    log.setLevel(logging.INFO)
+
+    handler = logging.FileHandler(filename=f'logs/main_qa_to_webserver.log')
+    file_formatter = logging.Formatter(fmt=LOGGER_FORMAT, datefmt=DATE_FORMAT)
+    handler.setFormatter(file_formatter)
+    log.addHandler(handler)
+
+    log.info(f'Starting new execution at {datetime.now().strftime("%Y_%m_%d_%H_%M")}')
+
     # Configuration parameters:
     CLUSTERNAME = 'cc-cedar'
-
-    RUN_CONTINUUM = True
-    RUN_LINES = True
 
     uname = 'ekoch'
 
     SHEETNAME = '20A - OpLog Summary'
+    # SHEETNAME = 'Archival Track Summary'
+
+    DO_DATA_TRANSFER = True
 
     # Specify a target to grab the QA products and process
-    TARGETS = ['IC10', 'NGC6822', 'M31']
+    TARGETS = ['IC10', 'NGC6822', 'M31', 'M33', 'IC1613', 'WLM']
 
-    MANUAL_EBID_LIST = [39742380]
-    # MANUAL_EBID_LIST = None
 
-    start_with_newest = True
+    MANUAL_EBID_LIST = []
+
+    # ebid, continuum, lines
+
+    MANUAL_EBID_LIST.append([40264132, True, False])
+
+    # MANUAL_EBID_LIST.append([41031266, False, True])
+
+    # MANUAL_EBID_LIST.append([40729151, True, True])
+
+
+    start_with_newest = False
 
     while True:
 
@@ -128,7 +176,7 @@ if __name__ == "__main__":
 
         loop = asyncio.new_event_loop()
 
-        loop.set_debug(False)
+        loop.set_debug(True)
         loop.slow_callback_duration = 0.001
 
         loop.run_until_complete(run(start_with_newest=start_with_newest,
