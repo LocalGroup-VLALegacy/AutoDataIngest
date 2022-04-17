2f09d511e34ad8a630f17b21cc20f04aa3a5654c
e-koch <koch.eric.w@gmail.com>
2022-04-17 17:47:46 -0400

    Switch over to the new aoc data staging area

--- autodataingest/cluster_configs.py
+++ autodataingest/cluster_configs.py
@@ -13,7 +13,7 @@ import autodataingest.job_templates.job_line_pipeline as jobs_line
 ENDPOINT_INFO = {'cc-cedar': {'endpoint_id': "c99fd40c-5545-11e7-beb6-22000b9a448b",
                            'data_path': "scratch/VLAXL_reduction/"},
                  'nrao-aoc': {'endpoint_id': "62708910-8e89-11e8-9641-0a6d4e044368",
-                              'data_path': '/lustre/aoc/projects/20A-346/data_staged/'},
+                              'data_path': '/lustre/aoc/projects/20A-346/data_staged_new/'},
                  'msu-hpcc': {'endpoint_id': "a640bafc-6d04-11e5-ba46-22000b92c6ec",
                              'data_path': "/mnt/research/ChomiukLab/LocalGroupX/M31_20A-346/"},
                  'ingester': {'endpoint_id': "e8fc98cc-9ca8-11eb-92cd-6b08dd67ff48",
