4b1ae432d663e9fe7f3a883129741fa2d23069e4
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-17 16:55:22 +0000

    Adding tarfile member sanitization to extractall()

--- autodataingest/generate_QA/make_new_QA_plots.py
+++ autodataingest/generate_QA/make_new_QA_plots.py
@@ -32,7 +32,26 @@ for track_tar in track_products:
 
     with tarfile.open(track_tar) as tf:
 
-        tf.extractall(path=folder_name)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tf, path=folder_name)
 
     # Identify the folder we need for QA:
     # 1. weblog
@@ -67,7 +86,26 @@ for track_tar in track_products:
             # Extract the tgz file to the QA output folder
             with tarfile.open(output_prod) as tf:
 
-                tf.extractall(path=qa_output)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tf, path=qa_output)
 
             # Delete the copied weblog.tgz
             output_prod.unlink()
