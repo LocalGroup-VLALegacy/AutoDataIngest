fa9f0b82b6bd66706ef3010b45eb584f4750982f
e-koch <koch.eric.w@gmail.com>
2023-05-03 19:09:35 -0400

    Updated script with new folder structure on the archive

--- flatten_and_tar_archival.sh
+++ flatten_and_tar_archival.sh
@@ -13,30 +13,55 @@ output_dir="/lustre/aoc/projects/20A-346/data_staged_new/"
 
 cd $folder_name
 
-for sdm_name in *.sb*.eb*;
+# New structure as of 04/2023:
+# Loop through project IDs
+
+for this_project in */;
     do
 
-    echo "Starting on ${sdm_name} at $(date +%F_%T)"
+    cd $this_project
 
-    if [ -f "$sdm_name" ]; then
-        continue
-    fi
+    echo "Starting on ${this_project} at $(date +%F_%T)"
 
-    # Rename nested folders
-    mv ${sdm_name} ${sdm_name}_temp
+    # Loop through the observations folder: "observation.MJD"
+    for this_obs in observation.*;
+        do
 
-    mv ${sdm_name}_temp/${sdm_name} .
+        echo "Starting on ${this_obs} at $(date +%F_%T)"
 
-    rmdir ${sdm_name}_temp
+        cd $this_obs
 
-    tar -cf ${sdm_name}.tar ${sdm_name}
+        # There should only be 1 SDM file, but it's easier to just "loop" here:
+        for sdm_name in *.sb*.eb*;
+            do
 
-    mv ${sdm_name}.tar $output_dir
+            echo "Starting on ${sdm_name} at $(date +%F_%T)"
 
-    rm -rf ${sdm_name}
+            if [ -f "$sdm_name" ]; then
+                continue
+            fi
 
-    echo "Finished with ${sdm_name} at $(date +%F_%T)"
+            # Rename nested folders
+            # mv ${sdm_name} ${sdm_name}_temp
+            # mv ${sdm_name}_temp/${sdm_name} .
+            # rmdir ${sdm_name}_temp
+
+            tar -cf ${sdm_name}.tar ${sdm_name}
+
+            mv ${sdm_name}.tar $output_dir
+
+            rm -rf ${sdm_name}
+
+            echo "Finished with ${sdm_name} at $(date +%F_%T)"
+
+        done
+
+        cd ../
 
     done
+
+    cd ../
+
+done
 
 echo "Finished all at $(date +%F_%T)"
